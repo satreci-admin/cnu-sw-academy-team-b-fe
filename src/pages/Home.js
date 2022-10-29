e5362b9c7e88224024cfff70a7b929c606747fd9
Kim-AYoung <onionlily123@naver.com>
2022-10-29 17:11:37 +0900

    fix: image에 alt 속성 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,10 @@ function Home() {
     <div className="hero">
       <div className="hero__bg">
         <picture>
-          <img src="https://images.unsplash.com/photo-1491982883790-ead7c97a047e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2594&q=80" />
+          <img
+            alt="background-img"
+            src="https://images.unsplash.com/photo-1491982883790-ead7c97a047e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2594&q=80"
+          />
         </picture>
       </div>
       <div className="hero__cnt">
